a5bbeb8c7aa9122b1a8734df2251e620e5e90890
Muhammad Salman <muhammad.salman@10pearls.com>
2022-04-25 10:37:57 +0500

    changes commit

--- UtilityProject/Program.cs
+++ UtilityProject/Program.cs
@@ -13,6 +13,7 @@ namespace UtilityProject
         [STAThread]
         static void Main()
         {
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
